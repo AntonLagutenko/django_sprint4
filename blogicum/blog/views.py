cafd37a019f053302080992259d24fd63393ab49
Anton Lagutenko <laglip@mail.ru>
2024-08-11 20:11:41 +0300

    test_post красный. Остальное во  views исправлено

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,14 +1,13 @@
 from constants.constants import AMOUNT_POSTS
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.paginator import Paginator
 from django.db.models import Count
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.utils import timezone
 from django.utils.timezone import now
-from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 
 from .forms import (
     CommentForm, PostForm, UserProfileForm
@@ -59,24 +58,19 @@ class DeletePostView(LoginRequiredMixin, DeleteView):
     pk_url_kwarg = 'post_id'
     template_name = 'blog/create.html'
 
-    def get_object(self, queryset=None):
-        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
-        if post.author != self.request.user:
-
-            raise Http404("У вас нет прав на удаление этого поста")
-        return post
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            obj = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        except Http404:
+            raise Http404("Публикация не найдена")
+        if obj.author != self.request.user:
+            return HttpResponseRedirect(reverse('blog:post_detail',
+                                                kwargs={'post_id': obj.pk}))
+        return super().dispatch(request, *args, **kwargs)
 
     def get_success_url(self):
         return reverse('blog:profile',
                        kwargs={'username': self.request.user.username})
-
-    def dispatch(self, request, *args, **kwargs):
-        try:
-            self.get_object()
-        except Http404:
-            return redirect('blog:post_detail',
-                            post_id=self.kwargs.get('post_id'))
-        return super().dispatch(request, *args, **kwargs)
 
 
 class EditProfileView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
@@ -100,26 +94,32 @@ class EditProfileView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return context
 
 
-class UserProfileView(DetailView):
-    model = User
-    template_name = 'blog/profile.html'
-    context_object_name = 'profile'
+class PostListMixin:
+    paginate_by = AMOUNT_POSTS
 
-    def get_object(self, queryset=None):
+    def get_queryset(self):
+        return Post.objects.annotate(
+            comment_count=Count('post_comments')).order_by('-pub_date')
+
+
+class UserProfileView(PostListMixin, ListView):
+    template_name = 'blog/profile.html'
+    context_object_name = 'page_obj'
+
+    def get_queryset(self):
         username = self.kwargs.get('username')
-        return get_object_or_404(self.model, username=username)
+        author = get_object_or_404(User, username=username)
+        if self.request.user == author:
+            return super().get_queryset().filter(author=author)
+        else:
+            return super().get_queryset().filter(author=author,
+                                                 is_published=True,
+                                                 pub_date__lte=timezone.now())
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        posts_list = Post.objects.filter(
-            author=self.object
-        ).annotate(
-            comment_count=Count('post_comments')
-        ).order_by('-pub_date')
-
-        paginator = Paginator(posts_list, AMOUNT_POSTS)
-        page = self.request.GET.get('page')
-        context['page_obj'] = paginator.get_page(page)
+        username = self.kwargs.get('username')
+        context['profile'] = get_object_or_404(User, username=username)
         return context
 
 
@@ -134,13 +134,15 @@ def get_published_posts():
 def index(request):
     post_db = get_published_posts().annotate(
         comments_count=Count('post_comments')
-    ).order_by('-pub_date')[:AMOUNT_POSTS]
-    for post in post_db:
+    ).order_by('-pub_date')
+    paginator = Paginator(post_db, AMOUNT_POSTS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    for post in page_obj:
         post.title = f"{post.title} ({post.comments_count})"
-    return render(request, "blog/index.html", {"page_obj": post_db})
+    return render(request, "blog/index.html", {"page_obj": page_obj})
 
 
-@login_required
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     if (not post.is_published
@@ -148,15 +150,7 @@ def post_detail(request, post_id):
             or post.pub_date > timezone.now()) and post.author != request.user:
         raise Http404("Post not found")
     comments = post.post_comments.all().order_by('created_at')
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            text = form.cleaned_data['text']
-            comment = Comment(author=request.user, post=post, text=text)
-            comment.save()
-            return redirect('post_detail', post_id=post_id)
-    else:
-        form = CommentForm()
+    form = CommentForm()
     context = {
         'post': post,
         'comments': comments,
@@ -168,10 +162,10 @@ def post_detail(request, post_id):
 def category_posts(request, category_slug):
     category = get_object_or_404(
         Category, slug=category_slug, is_published=True)
-    if not category.is_published:
-        raise Http404("Post not found")
-    post_list = get_published_posts().filter(category=category)
-    paginator = Paginator(post_list, 10)
+    post_list = get_published_posts().filter(category=category).annotate(
+        comments_count=Count('post_comments')
+    ).order_by('-pub_date')
+    paginator = Paginator(post_list, AMOUNT_POSTS)
     page = request.GET.get('page')
     page_obj = paginator.get_page(page)
     return render(request,
@@ -190,21 +184,9 @@ class EditCommentView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return reverse('blog:post_detail',
                        kwargs={'post_id': comment.post.pk})
 
-    def get_object(self):
-        comment_id = self.kwargs.get('comment_id')
-        return get_object_or_404(Comment, pk=comment_id)
-
     def test_func(self):
         comment = self.get_object()
-        return (
-            self.request.user.is_authenticated
-            and self.request.user == comment.author
-        )
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['comment'] = self.get_object()
-        return context
+        return self.request.user == comment.author
 
 
 class DeleteCommentView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
@@ -216,50 +198,25 @@ class DeleteCommentView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         post_id = self.kwargs.get('post_id')
         return reverse('blog:post_detail', kwargs={'post_id': post_id})
 
-    def get_object(self):
-        comment_id = self.kwargs.get('comment_id')
-        return get_object_or_404(Comment, pk=comment_id)
-
     def test_func(self):
         comment = self.get_object()
-        return (
-            self.request.user.is_authenticated
-            and self.request.user == comment.author
-        )
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['comment'] = self.get_object()
-        return context
-
-    def dispatch(self, request, *args, **kwargs):
-        try:
-            post = self.get_object()
-        except Post.DoesNotExist:
-            raise Http404("Пост не найден")
-        if post.author != request.user and not request.user.is_staff:
-            return redirect('blog:post_detail', post_id=post.pk)
-        return super().dispatch(request, *args, **kwargs)
+        return self.request.user == comment.author
 
 
 class AddCommentView(LoginRequiredMixin, CreateView):
     model = Comment
     form_class = CommentForm
     template_name = 'blog/comment.html'
-    post_obj = None
-
-    def dispatch(self, request, *args, **kwargs):
-        try:
-            self.post_obj = Post.objects.get(pk=self.kwargs.get('post_id'))
-        except Post.DoesNotExist:
-            return render(request, 'pages/404.html', status=404)
-        return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
-        form.instance.post = self.post_obj
+        try:
+            post_obj = Post.objects.get(pk=self.kwargs.get('post_id'))
+        except Post.DoesNotExist:
+            return render(self.request, 'pages/404.html', status=404)
+        form.instance.post = post_obj
         form.instance.author = self.request.user
         return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('blog:post_detail',
-                       kwargs={'post_id': self.post_obj.pk})
+                       kwargs={'post_id': self.kwargs.get('post_id')})
